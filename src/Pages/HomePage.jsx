a3656c1907a1d76f5cc77f39b267eab7570d30b2
Nisha Patil <147735151+patilnisha02@users.noreply.github.com>
2024-06-19 14:26:57 +0200

    css added to main title

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import BookingTable from '../Components/BookingTable';
 const HomePage = () => {
   return (
     <>
-    <h3>Welcome to Smashers Connect Club</h3>
+    <h1 className='color-change-title '>Welcome to Smashers Connect Club</h1>
     <img className="image" src="https://cdn.pixabay.com/photo/2016/05/31/23/21/badminton-1428047_640.jpg" alt="badminton" />
     
      <BookingTable/>
